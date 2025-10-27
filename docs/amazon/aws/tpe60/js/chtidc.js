29c310fc095284544415f5dba172fcb759bd93db
Charlie Wang <32430037+CRE0809@users.noreply.github.com>
2025-10-27 09:25:55 +0800

    Update

--- docs/amazon/aws/tpe60/js/chtidc.js
+++ docs/amazon/aws/tpe60/js/chtidc.js
@@ -69,16 +69,16 @@ async function copyPages(dateStart,dateEnd,people) {
     // 第二頁
     var [firstDonorPage] = await pdfDoc.copyPages(firstDonorPdfDoc, [1])
     pdfDoc.addPage(firstDonorPage);
-    firstDonorPage.drawText(`${dateStart} 08 時 起`, { x: 140, y: 710, size: 12, font: customFont, color: rgb(0,0,0)});
-    firstDonorPage.drawText(`${dateEnd} 19 時 止`, { x: 140, y: 690, size: 12, font: customFont, color: rgb(0,0,0)});
+    firstDonorPage.drawText(`${dateStart} 08 時 起`, { x: 125, y: 710, size: 12, font: customFont, color: rgb(0,0,0)});
+    firstDonorPage.drawText(`${dateEnd} 19 時 止`, { x: 125, y: 690, size: 12, font: customFont, color: rgb(0,0,0)});
     // 承攬廠商
     firstDonorPage.drawText(`DB Schenker`, { x: 125, y: 660, size: 10, font: customFont, color: rgb(0,0,0)});
     // 負責人
-    firstDonorPage.drawText(`王俊嵐`, { x: 125, y: 640, size: 10, font: customFont, color: rgb(0,0,0)});
+    firstDonorPage.drawText(`王俊嵐`, { x: 125, y: 630, size: 10, font: customFont, color: rgb(0,0,0)});
     // 工作場所負責人
-    firstDonorPage.drawText(`王俊嵐`, { x: 125, y: 620, size: 10, font: customFont, color: rgb(0,0,0)});
+    firstDonorPage.drawText(`王俊嵐`, { x: 125, y: 610, size: 10, font: customFont, color: rgb(0,0,0)});
     // 連絡電話
-    firstDonorPage.drawText(`03-3852500 分機 401`, { x: 125, y: 600, size: 10, font: customFont, color: rgb(0,0,0)});
+    firstDonorPage.drawText(`03-3852500 分機 401`, { x: 125, y: 580, size: 10, font: customFont, color: rgb(0,0,0)});
 
     var yyy = 385;
     for(var i in people) {
